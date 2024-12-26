cc982b5ebb37abdeea6e087329f5ede81a35b6a2
fathi-asma <fathuasma456@gmail.com>
2024-12-26 11:21:06 +0530

    Add files via upload

--- checkPrimeOrNot.java
+++ checkPrimeOrNot.java
@@ -1,34 +1,43 @@
 import java.util.Scanner;
-public class checkPrimeOrNot{
-	public static void main(String args[]){
-	Scanner input=new Scanner(System.in);
 
-	        System.out.print("Enter the number :");
-	   	int number=input.nextInt();
+public class checkPrimeOrNot {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
 
-		if (number<0) {
-		System.out.println("Enter the non negative number:");
-	       } else {	
-		int factorcounter=0;
+        // Prompt user to enter a number
+        System.out.print("Enter the number: ");
+        int number = input.nextInt();
 
-                for(int j=1; j<=number; j++){
-                if(number%j==0) {
-                factorcounter++;
-	       }
-           }
-   System.out.println("Number: "+ number);
-   System.out.println("Factor counte:"+factorcounter);
+        // Line space for correct formatting
+        System.out.println();
 
-  if (number==0|| number ==1) {
-	System.out.println("Status : not a primary number");
- } else if(factorcounter==2) {
-	System.out.println("Status : primary number");
-} else {
-	System.out.println("Status : not a primary number");
-	}
- } 
-input.close();
-	}
+        // Check if the entered number is negative or positive
+        if (number < 0) {
+            System.out.println("Enter a non-negative number:");
+        } else {
+            int factorCounter = 0; // Count factors
+
+            // Loop for counting the number of factors
+            for (int j = 1; j <= number; j++) {
+                if (number % j == 0) {
+                    factorCounter++;
+                }
+            }
+
+            // Display the number and its factors
+            System.out.println("Number: " + number);
+            System.out.println("Factor count: " + factorCounter);
+
+            // Determine if the entered number is prime or not
+            if (number == 0 || number == 1) {
+                System.out.println("Status: Non-Primary number");
+            } else if (factorCounter == 2) {
+                System.out.println("Status: Primary number");
+            } else {
+                System.out.println("Status: Not a prime number");
+            }
+        }
+
+        input.close();
+    }
 }
-
-				
